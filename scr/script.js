73e17f87d1656115231ce94aebd430ae2e058b83
Nixian001 <144830699+Nixian001@users.noreply.github.com>
2026-01-23 10:08:11 +0000

    Add rotating text

--- scr/script.js
+++ scr/script.js
@@ -1,7 +1,15 @@
+const GOODDAY_TEXTS = [
+    "Hello, World!", "Heló, Világ."
+]
+gdt_idx = 0;
+const html_label = document.getElementById('good-morning-label');
+
 function main() {
     setInterval(setText, 2500);
 }
 
-function setText(text) {
-    print("set text");
+function setText() {
+    gdt_idx = gdt_idx % GOODDAY_TEXTS.length;
+    html_label.innerHTML = GOODDAY_TEXTS[gdt_idx];
+    gdt_idx = gdt_idx + 1;
 }

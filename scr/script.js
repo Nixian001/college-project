6721263f0ef0dc3e9339140728802520da903b6c
Nixian001 <144830699+Nixian001@users.noreply.github.com>
2026-01-23 13:58:53 +0000

    add fading to the text.

--- scr/script.js
+++ scr/script.js
@@ -6,12 +6,30 @@ const html_label = document.getElementById("good-morning-label");
 
 function main() {
     setText();
-    setInterval(setText, 2500);
+    setInterval(setText, 5000);
 }
 
-function setText() {
+async function fadeText(startalpha, endalpha) {
+    var t = 0;
+
+    while (t < 1.0) {
+        html_label.style.opacity = lerp(startalpha, endalpha, t);
+        setTimeout(20);
+        t += 0.02;
+    }
+
+    return;
+}
+
+function lerp(a, b, t) {
+    return a + (b - a) * t;
+}
+
+async function setText() {
     gdt_idx = gdt_idx % GOODDAY_TEXTS.length;
+    await fadeText(1, 0);
     html_label.innerText = GOODDAY_TEXTS[gdt_idx];
+    await fadeText(0, 1);
     gdt_idx = gdt_idx + 1;
 }
 
